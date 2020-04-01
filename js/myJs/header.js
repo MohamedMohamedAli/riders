4e5c423fc85fdd0d79ffefbaaaf2b21b0a31b198
Mohamed <Mohamed_Mohamed-Ali@hotmail.com>
2020-04-01 02:52:32 +0200

    aggiunta header

---
+++ js/myJs/header.js
@@ -0,0 +1,19 @@
+document.write('\
+<nav class="navbar navbar-expand-lg navbar-light " style="background-color: black; position:fixed; width:100%;z-index:10;" >\
+	<a class="navbar-brand" href="index.html" style="color:rgb(255, 230, 0)">Riders</a>\
+</nav>\
+<h1 style="color:white">-</h1>\
+ \
+ <script src="js/jquery-1.10.2.min.js"></script>\
+ <script src="js/bootstrap.min.js"></script>\
+ <script src="js/bootstrap.bundle.js"></script>\
+ <script src="js/jqueryUI/jquery-ui.min.js"></script>\
+ <link href="css/bootstrap.min.css" rel="stylesheet">\
+ <link href="css/mycss/mycss.css" rel="stylesheet">\
+ <style>\
+ td {\
+     text-align: left;\
+     padding: 10px 20px;\
+ }\
+</style>\
+');
